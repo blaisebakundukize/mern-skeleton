bb10770ff362b9c147db51985b3775fb5d0ebc59
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-17 18:29:44 +0200

    added home component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import Home from "./core/Home";
 
 const MainRouter = () => {
   return (
     <div>
-      <switch></switch>
+      <switch>
+        <Route exact path='/' component={Home} />
+      </switch>
     </div>
   );
 };
