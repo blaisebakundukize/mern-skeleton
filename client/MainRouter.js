65201b463a4292afb17a63f06ee47412a325b948
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-18 08:37:51 +0200

    added users component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./core/Home";
+import Users from "./user/Users";
 
 const MainRouter = () => {
   return (
     <div>
-      <switch>
+      <Switch>
         <Route exact path='/' component={Home} />
-      </switch>
+        <Route path='/users' component={Users} />
+      </Switch>
     </div>
   );
 };
