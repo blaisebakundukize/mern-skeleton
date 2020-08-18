18853bedc4a39775635af208b1f75d0afe884ab3
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-18 10:25:52 +0200

    added edit profile component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import PrivateRoute from "./auth/PrivateRoute";
 import Home from "./core/Home";
 import Users from "./user/Users";
 import Signup from "./user/Signup";
 import Signin from "./auth/Signin";
 import Profile from "./user/Profile";
+import EditProfile from "./user/EditProfile";
 
 const MainRouter = () => {
   return (
@@ -14,6 +16,7 @@ const MainRouter = () => {
         <Route path='/users' component={Users} />
         <Route path='/signup' component={Signup} />
         <Route path='/signin' component={Signin} />
+        <PrivateRoute path='/user/edit/:userId' component={EditProfile} />
         <Route path='/user/:userId' component={Profile} />
       </Switch>
     </div>
