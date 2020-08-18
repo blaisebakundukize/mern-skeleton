d256e327aaeb91131668298e3dee2e90c8d0bd97
Blaise Bakundukize <blaisebakundukize@gmail.com>
2020-08-18 09:44:44 +0200

    added signin component

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -12,6 +12,7 @@ const MainRouter = () => {
         <Route exact path='/' component={Home} />
         <Route path='/users' component={Users} />
         <Route path='/signup' component={Signup} />
+        <Route path='/signin' component={Signin} />
       </Switch>
     </div>
   );
